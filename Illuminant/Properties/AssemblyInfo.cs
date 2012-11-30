0f4c1ce17d39e561ac558df02327872b9fbdea19
Kevin Gadd <kevin.gadd@gmail.com>
2012-11-30 12:51:01 -0800

    Empty project skeleton

---
+++ Illuminant/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Squared.Illuminant")]
+[assembly: AssemblyProduct("Squared.Illuminant")]
+[assembly: AssemblyDescription("Squared.Illuminant Realtime Lighting Engine")]
+[assembly: AssemblyCompany("Squared Interactive")]
+[assembly: AssemblyCopyright("Copyright © Kevin Gadd 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("9ea66d93-43fb-417b-8d96-95da1eb9d2c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
