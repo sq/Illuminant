71b525af616f9d293fe4b2e94af88b53146c494a
Katelyn Gadd <kg@luminance.org>
2018-01-16 03:19:05 -0800

    Significant updates to brightness estimation

--- Illuminant/LightingRenderer.cs
+++ Illuminant/LightingRenderer.cs
@@ -367,9 +367,24 @@ namespace Squared.Illuminant {
         private const int GreenScaleFactor = (int)(0.587 * LuminanceScaleFactor);
         private const int BlueScaleFactor = (int)(0.114 * LuminanceScaleFactor);
 
+        private int[] AnalyzeScratchBuffer;
+
+        private float ComputePercentile (float percentage, int[] buffer, int lastZero, int count, float effectiveScaleFactor) {
+            count -= lastZero;
+            var index = (int)(count * percentage / 100f);
+            if (index < 0)
+                index = 0;
+            if (index >= count)
+                index = count - 1;
+
+            var sample = buffer[lastZero + index];
+            return sample * effectiveScaleFactor;
+        }
+
         private unsafe LightmapInfo AnalyzeLightmap (
             byte[] buffer, int count, 
-            float scaleFactor, float threshold
+            float scaleFactor, float threshold,
+            float lowBandPercentage, float highBandPercentage
         ) {
             int overThresholdCount = 0, min = int.MaxValue, max = 0;
             long sum = 0;
@@ -379,6 +394,9 @@ namespace Squared.Illuminant {
                     ? 8
                     : 4);
 
+            if ((AnalyzeScratchBuffer == null) || (AnalyzeScratchBuffer.Length < pixelCount))
+                AnalyzeScratchBuffer = new int[pixelCount];
+
             int luminanceThreshold = (int)(threshold * LuminanceScaleFactor / scaleFactor);
 
             fixed (byte* pBuffer = buffer)
@@ -387,6 +405,8 @@ namespace Squared.Illuminant {
 
                 for (int i = 0; i < pixelCount; i++, pRgba += 4) {
                     int luminance = ((pRgba[0] * RedScaleFactor) + (pRgba[1] * GreenScaleFactor) + (pRgba[2] * BlueScaleFactor)) / 65536;
+                    AnalyzeScratchBuffer[i] = luminance;
+
                     min = Math.Min(min, luminance);
                     max = Math.Max(max, luminance);
                     sum += luminance;
@@ -399,24 +419,72 @@ namespace Squared.Illuminant {
 
                 for (int i = 0; i < pixelCount; i++, pRgba += 4) {
                     int luminance = ((pRgba[0] * 257 * RedScaleFactor) + (pRgba[1] * 257 * GreenScaleFactor) + (pRgba[2] * 257 * BlueScaleFactor)) / 65536;
+                    AnalyzeScratchBuffer[i] = luminance;
+
                     min = Math.Min(min, luminance);
                     max = Math.Max(max, luminance);
                     sum += luminance;
 
                     if (luminance >= luminanceThreshold)
                         overThresholdCount += 1;
-
                 }                
             }
 
             var effectiveScaleFactor = (1.0f / LuminanceScaleFactor) * scaleFactor;
 
-            return new LightmapInfo {
+            var scratch = AnalyzeScratchBuffer;
+            Array.Sort(scratch, 0, pixelCount);
+            var lastZero = Array.LastIndexOf(scratch, 0);
+
+            var percentiles = new LightmapPercentileInfo {
+                P1 = ComputePercentile(1, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P2 = ComputePercentile(2, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P5 = ComputePercentile(5, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P10 = ComputePercentile(10, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P25 = ComputePercentile(25, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P50 = ComputePercentile(50, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P75 = ComputePercentile(75, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P90 = ComputePercentile(90, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P95 = ComputePercentile(95, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P98 = ComputePercentile(98, scratch, lastZero, pixelCount, effectiveScaleFactor),
+                P99 = ComputePercentile(99, scratch, lastZero, pixelCount, effectiveScaleFactor)
+            };
+
+            var result = new LightmapInfo {
+                Percentiles = percentiles,
                 Overexposed = overThresholdCount / (float)pixelCount,
                 Minimum = min * effectiveScaleFactor,
                 Maximum = max * effectiveScaleFactor,
                 Mean = (sum * effectiveScaleFactor) / pixelCount
             };
+
+            int bandCount = 0, 
+                bandMin = (int)ComputePercentile(lowBandPercentage, scratch, lastZero, pixelCount, 1),
+                bandMax = (int)ComputePercentile(highBandPercentage, scratch, lastZero, pixelCount, 1);
+            min = int.MaxValue;
+            max = 0;
+            sum = 0;
+
+            for (int i = lastZero; i < pixelCount; i++) {
+                var luminance = scratch[i];
+                if ((luminance < bandMin) || (luminance > bandMax))
+                    continue;
+                min = Math.Min(min, luminance);
+                max = Math.Max(max, luminance);
+                sum += luminance;
+                bandCount++;
+            }
+
+            if (min == int.MaxValue)
+                min = 0;
+
+            result.Band = new LightmapBandInfo {
+                Minimum = min * effectiveScaleFactor,
+                Maximum = max * effectiveScaleFactor,
+                Mean = (sum * effectiveScaleFactor / bandCount)
+            };
+
+            return result;
         }
 
         /// <summary>
@@ -430,6 +498,8 @@ namespace Squared.Illuminant {
         public void EstimateBrightness (
             Action<LightmapInfo> onComplete,
             float scaleFactor, float threshold, 
+            float lowBandPercentage = 70,
+            float highBandPercentage = 90,
             int accuracyFactor = 3
         ) {
             if (!Configuration.EnableBrightnessEstimation)
@@ -453,7 +523,10 @@ namespace Squared.Illuminant {
                     _ReadbackBuffer, 0, count
                 );
 
-                var result = AnalyzeLightmap(_ReadbackBuffer, count, scaleFactor, threshold);
+                var result = AnalyzeLightmap(
+                    _ReadbackBuffer, count, scaleFactor, threshold,
+                    lowBandPercentage, highBandPercentage
+                );
                 onComplete(result);
             });
         }
@@ -1727,7 +1800,18 @@ namespace Squared.Illuminant {
         ToneMap
     }
 
+    public struct LightmapPercentileInfo {
+        public float P1, P2, P5, P10, P25, P50, P75, P90, P95, P98, P99;
+    }
+
+    public struct LightmapBandInfo {
+        public float Minimum, Maximum, Mean;
+    }
+
     public struct LightmapInfo {
+        public LightmapPercentileInfo Percentiles;
+        public LightmapBandInfo Band;
+
         public float Minimum, Maximum, Mean;
         public float Overexposed;
     }
