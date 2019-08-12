fe57111e20e594dca37f8b5270afe1d407d22f58
Katelyn Gadd <kg@luminance.org>
2019-08-12 09:46:28 -0700

    Update test app

--- TestGame/TestGame/Scenes/2DShapes.cs
+++ TestGame/TestGame/Scenes/2DShapes.cs
@@ -31,8 +31,10 @@ namespace TestGame.Scenes {
             : base(game, width, height) {
             Gamma.Min = 0.1f;
             Gamma.Max = 3.0f;
-            Gamma.Value = 1.5f;
+            Gamma.Value = 1.0f;
             Gamma.Speed = 0.1f;
+            BlendInLinearSpace.Value = true;
+            Outlines.Value = true;
         }
 
         public override void LoadContent () {
@@ -53,13 +55,13 @@ namespace TestGame.Scenes {
             ir.RasterizeEllipse(
                 Vector2.One * 500, Vector2.One * 420, Outlines ? 1f : 0, 
                 new Color(0.0f, 0.0f, 0.0f, 1f), 
-                new Color(0.1f, 0.1f, 0.1f, 1f), 
+                new Color(0.2f, 0.2f, 0.2f, 1f), 
                 outlineColor: Color.White, 
                 layer: 1
             );
 
             ir.RasterizeLineSegment(
-                new Vector2(32, 32), new Vector2(1024, 64), Vector2.One * 8, Outlines ? 1f : 0f, 
+                new Vector2(32, 32), new Vector2(1024, 64), 8, Outlines ? 1f : 0f, 
                 Color.White, Color.Black,
                 outlineColor: Color.Red,
                 gradientAlongLine: GradientAlongLine, 
@@ -69,7 +71,7 @@ namespace TestGame.Scenes {
             var tl = new Vector2(80, 112);
             var br = new Vector2(512, 400);
             ir.RasterizeRectangle(
-                tl, br, Vector2.One * (AnimateRadius.Value 
+                tl, br, (AnimateRadius.Value 
                     ? Arithmetic.PulseSine(now / 3f, 0, 32)
                     : 0f), Outlines ? 6f : 0f, 
                 Color.Red, Color.Green,
@@ -80,33 +82,33 @@ namespace TestGame.Scenes {
             );
 
             ir.RasterizeRectangle(
-                new Vector2(16, 256), new Vector2(16, 512), Vector2.One * 4, new Color(1f, 0, 0, 1), new Color(0.5f, 0, 0, 1),
+                new Vector2(16, 256), new Vector2(16, 512), 8, new Color(1f, 0, 0, 1), new Color(0.5f, 0, 0, 1),
                 layer: 2
             );
 
             ir.RasterizeRectangle(
-                new Vector2(32, 256), new Vector2(32, 512), Vector2.One * 4, new Color(1f, 1f, 0, 1), new Color(0.5f, 0.5f, 0, 1),
+                new Vector2(32, 256), new Vector2(32, 512), 8, new Color(1f, 1f, 0, 1), new Color(0.5f, 0.5f, 0, 1),
                 layer: 2
             );
 
             ir.RasterizeRectangle(
-                new Vector2(48, 256), new Vector2(48, 512), Vector2.One * 4, new Color(0f, 1f, 0, 1), new Color(0f, 0.5f, 0, 1),
+                new Vector2(48, 256), new Vector2(48, 512), 8, new Color(0f, 1f, 0, 1), new Color(0f, 0.5f, 0, 1),
                 layer: 2
             );
 
             ir.RasterizeRectangle(
-                new Vector2(64, 256), new Vector2(64, 512), Vector2.One * 4, new Color(0f, 1f, 1f, 1), new Color(0f, 0.5f, 0.5f, 1),
+                new Vector2(64, 256), new Vector2(64, 512), 8, new Color(0f, 1f, 1f, 1), new Color(0f, 0.5f, 0.5f, 1),
                 layer: 2
             );
 
             ir.RasterizeRectangle(
-                new Vector2(80, 256), new Vector2(80, 512), Vector2.One * 4, new Color(0f, 0f, 1f, 1), new Color(0f, 0f, 0.5f, 1),
+                new Vector2(80, 256), new Vector2(80, 512), 8, new Color(0f, 0f, 1f, 1), new Color(0f, 0f, 0.5f, 1),
                 layer: 2
             );
 
             ir.RasterizeTriangle(
                 new Vector2(640, 96), new Vector2(1200, 256), new Vector2(800, 512), 
-                Vector2.One * 1, Outlines ? 1f : 0,
+                1, Outlines ? 1f : 0,
                 Color.Black, Color.White, outlineColor: Color.Blue,
                 layer: 2,
                 texture: UseTexture ? Texture : null
@@ -122,7 +124,7 @@ namespace TestGame.Scenes {
                 b = new Vector2(1200, 64);
 
             ir.RasterizeQuadraticBezier(
-                a, b, c, Vector2.One * 8, Outlines ? 1f : 0f, Color.White, Color.Black, Color.Red,
+                a, b, c, 8, Outlines ? 1f : 0f, Color.White, Color.Black, Color.Red,
                 layer: 3
             );
 
